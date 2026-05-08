54ab44d55cdf402ab79c2c9e1d8ed8b88d6e5511
piy-ush-20 <piyushsagar1120@gmail.com>
2026-05-08 19:50:36 +0530

    Change log: -Add login and home screen with Jetpack Compose - Add app launch icon

--- app/src/main/java/com/piyush/thoughtflow/MainActivity.kt
+++ app/src/main/java/com/piyush/thoughtflow/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import com.piyush.thoughtflow.ui.LoginScreen
+import com.piyush.thoughtflow.ui.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            MaterialTheme {
+//                LoginScreen()
+                HomeScreen()
+            }
         }
     }
 }
